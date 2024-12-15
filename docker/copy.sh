ed00545394ef97a26e39fba4a9a8e3fbd2022a00
darryl west <darryl.west@RainCitySoftware.com>
2024-12-15 10:06:33 -0800

    fixed the copy.sh to point to the correct service

--- docker/copy.sh
+++ docker/copy.sh
@@ -8,10 +8,10 @@ set -eu
 # docker cp ../.ssh/cert.pem cryptor-service:/home/cryptor/.ssh/
 # docker cp ../.ssh/key.pem cryptor-service:/home/cryptor/.ssh/
 
-docker cp /usr/local/include/ansi_colors.hpp config-service:/usr/local/include/
-docker cp /usr/local/include/httplib.h config-service:/usr/local/include/
+# docker cp /usr/local/include/ansi_colors.hpp cryptor-service:/usr/local/include/
+# docker cp /usr/local/include/httplib.h cryptor-service:/usr/local/include/
 
-docker cp ~/.ssh/id_rsa config-service:/home/dpw/.ssh/
-docker cp ~/.ssh/id_rsa.pub config-service:/home/dpw/.ssh/
+docker cp ~/.ssh/id_rsa cryptor-service:/home/cryptor/.ssh/
+docker cp ~/.ssh/id_rsa.pub cryptor-service:/home/cryptor/.ssh/
 
 
