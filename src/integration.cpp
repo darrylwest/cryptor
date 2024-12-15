7d43b2cd0d0d1f989163edbab55eac766d528a1d
darryl west <darryl.west@raincitysoftware.com>
2024-12-15 14:34:02 -0800

    implemented pipe (popen) for integration tests

--- src/integration.cpp
+++ src/integration.cpp
@@ -10,8 +10,12 @@
 #include <cassert>
 #include <csignal>
 #include <cstdlib>
+#include <cstdio>
 #include <iostream>
 #include <thread>
+#include <atomic>
+#include <csignal>
+#include <unistd.h>
 
 #include "testlib.hpp"
 #include "version.hpp"
@@ -19,13 +23,29 @@
 using namespace colors;
 
 // Define the function to start the service
-void run_server(std::atomic<bool>& running) {
+void run_server(std::atomic<bool>& running, const std::string& log_file) {
     running = true;
 
-    // Start the service in a separate process
-    std::system("build/cryptor --level 1 --base html --port 22022 &");
+    // Open a pipe to start the service
+    std::string command = "./build/cryptor --base html/ --port 22022 --level 0 > " + log_file + " 2>&1 & echo $!";
+    FILE* pipe = popen(command.c_str(), "r");
+    if (!pipe) {
+        std::cerr << "Failed to start service.\n";
+        running = false;
+        return;
+    }
 
-    std::cout << cyan << "service running..." << reset << std::endl;
+    // Read the process ID of the started service
+    char buffer[128];
+    if (fgets(buffer, sizeof(buffer), pipe) != nullptr) {
+        pid_t pid = std::stoi(buffer);
+        std::cout << "Service started with PID: " << pid << "\n";
+    } else {
+        std::cerr << "Failed to retrieve PID of the service.\n";
+        running = false;
+    }
+
+    pclose(pipe);
 
     // Wait for a brief period to allow the service to initialize
     std::this_thread::sleep_for(std::chrono::seconds(1));
@@ -34,18 +54,19 @@ void run_server(std::atomic<bool>& running) {
 // use cxxopts to parse host and port
 // if the service is not local (localhost or 127.0.0.1), then skip the startup?
 int main(int argc, char* argv[]) {
-    spdlog::set_level(spdlog::level::critical);
-
-    std::string msg = "Cryptor Server Unit Tests, Version: ";
-    std::cout << cyan << msg << yellow << cryptor::Version() << reset << "\n" << std::endl;
-
     std::atomic<bool> server_running(false);
-    std::thread server_thread(run_server, std::ref(server_running));
+    const std::string log_file = "service.log";
 
-    // Wait for the server to start
+    // start the server thread
+    std::thread server_thread(run_server, std::ref(server_running), log_file);
+
+     // Wait for the server to start
     while (!server_running) {
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
+
+    std::string msg = "Cryptor Server Unit Tests, Version: ";
+    std::cout << cyan << msg << yellow << cryptor::Version() << reset << "\n" << std::endl;
 
     // Create a client for testing
     httplib::Client cli("https://localhost:22022");
