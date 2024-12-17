705acd9531f3b4508d715db2cb6ba3b50d9d68c4
darryl west <darryl.west@RainCitySoftware.com>
2024-12-17 15:17:58 -0800

    modified post-install to cp pems to dev/.ssh

--- docker/home/cryptor/post-install.sh
+++ docker/home/cryptor/post-install.sh
@@ -8,6 +8,8 @@ set -eu
 # make sure the id_rsa keys are copied first... 
 git clone git@github.com:darrylwest/cryptor.git
 
+cp  ~/.ssh/*.pem cryptor/cryptor/.ssh
+
 [ -x .cargo/bin/cargo ] || {
     curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh -s -- -y
     cargo install lsd
